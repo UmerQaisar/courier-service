79fd76c3c995c121797adbde8b213a3d850ecc91
Umer Qaisar <umerqaisar786@gmail.com>
2022-08-19 18:15:03 +0500

    Created items table

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   # Associations
   belongs_to :travel_detail
   belongs_to :user
+  has_many :items, dependent: :destroy
 
   # scopes
   scope :get_current_user_orders, ->(current_user_id) { where(:user_id => current_user_id)}
